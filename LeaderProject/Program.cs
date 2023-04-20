d5f0aad99a261bd694fb3dfea7c57853b86551df
Amirhoseinbabaie <c.amirgit@gmail.com>
2023-04-20 16:14:19 +0430

    Variables changed

--- LeaderProject/Program.cs
+++ LeaderProject/Program.cs
@@ -16,66 +16,66 @@ namespace Leader
             driver.Navigate().GoToUrl("https://www.leader.ir/");
             Console.WriteLine("opened browser");
 
-            var es = driver.FindElements(By.XPath("/html/body/footer/nav[1]/div/div[2]/div/div[1]/div[1]/ul/li"));
-            int x = es.Count();
-            List<string> esText = new List<string>();
+            var dataList = driver.FindElements(By.XPath("/html/body/footer/nav[1]/div/div[2]/div/div[1]/div[1]/ul/li"));
+            int countDataListTopics = dataList.Count();
+            List<string> listOfTopics = new List<string>();
 
-            for (int i = 1; i <= x; i++)
+            for (int i = 1; i <= countDataListTopics; i++)
             {
-                var e = driver.FindElement(By.XPath($"/html/body/footer/nav[1]/div/div[2]/div/div[1]/div[1]/ul/li[{i}]")).Text;
-                Console.WriteLine(e);
-                esText.Add(e);
+                var topics = driver.FindElement(By.XPath($"/html/body/footer/nav[1]/div/div[2]/div/div[1]/div[1]/ul/li[{i}]")).Text;
+                Console.WriteLine(topics);
+                listOfTopics.Add(topics);
 
 
-                if (e == "بیانات")
+                if (topics == "بیانات")
                 {
-                    var ce = driver.FindElement(By.XPath($"/html/body/footer/nav[1]/div/div[2]/div/div[1]/div[1]/ul/li[{i}]"));
+                    var selectTopics = driver.FindElement(By.XPath($"/html/body/footer/nav[1]/div/div[2]/div/div[1]/div[1]/ul/li[{i}]"));
                     Thread.Sleep(1000);
-                    ce.Click();
+                    selectTopics.Click();
                 }
             }
 
-            var n = driver.FindElements(By.XPath("/html/body/main/div[1]/section[1]/main/div[2]/ul/li"));
-            var ts = driver.FindElements(By.XPath("/html/body/main/div[1]/section[1]/main/div[3]/ul/li"));
+            var years = driver.FindElements(By.XPath("/html/body/main/div[1]/section[1]/main/div[2]/ul/li"));
+            var month = driver.FindElements(By.XPath("/html/body/main/div[1]/section[1]/main/div[3]/ul/li"));
 
-            for (int i = 2; i <= 2; i++)
+            for (int i = 2; i <= years.Count; i++)
             {
-                var t = driver.FindElement(By.XPath($"/html/body/main/div[1]/section[1]/main/div[2]/ul/li[{i}]"));
+                var selectedYear = driver.FindElement(By.XPath($"/html/body/main/div[1]/section[1]/main/div[2]/ul/li[{i}]"));
                 Thread.Sleep(1000);
-                t.Click();
+                selectedYear.Click();
 
-                for (int j = 1; j <= ts.Count; j++)
+                for (int j = 1; j <= month.Count; j++)
                 {
                     Thread.Sleep(1000);
-                    var st = driver.FindElement(By.XPath($"/html/body/main/div[1]/section[1]/main/div[3]/ul/li[{j}]"));
+                    var selectedMonth = driver.FindElement(By.XPath($"/html/body/main/div[1]/section[1]/main/div[3]/ul/li[{j}]"));
                     Thread.Sleep(1000);
-                    st.Click();
-                    Thread.Sleep(1000);
-
-                    var nl = driver.FindElements(By.XPath("/html/body/main/div[1]/section[2]/main/ul"));
+                    selectedMonth.Click();
                     Thread.Sleep(1000);
 
-                    if (nl.Count == 0)
+                    var statements = driver.FindElements(By.XPath("/html/body/main/div[1]/section[2]/main/ul"));
+                    Thread.Sleep(1000);
+
+                    if (statements.Count == 0)
                     {
                         driver.Navigate().Refresh();
                     }
 
-                    for (int k = 1; k <= nl.Count; k++)
+                    for (int k = 1; k <= statements.Count; k++)
                     {
                         Thread.Sleep(1000);
 
-                        var nk = driver.FindElement(By.XPath($"/html/body/main/div[1]/section[2]/main/ul[{k}]/li/div[2]/h6/a[2]"));
-                        nk.Click();
+                        var selectedStatements = driver.FindElement(By.XPath($"/html/body/main/div[1]/section[2]/main/ul[{k}]/li/div[2]/h6/a[2]"));
+                        selectedStatements.Click();
 
 
-                        var te = driver.FindElement(By.XPath("/html/body/main/div[1]/section/main/article[1]/div[1]/time/h6")).Text;
-                        var ti = driver.FindElement(By.XPath("/html/body/main/div[1]/section/main/article[1]/div[2]/h3")).Text;
-                        var no = driver.FindElement(By.XPath("/html/body/main/div[1]/section/main/article[2]/div[2]")).Text;
-                        te = new string(te.Where(c => !char.IsPunctuation(c)).ToArray());
+                        var newsTime = driver.FindElement(By.XPath("/html/body/main/div[1]/section/main/article[1]/div[1]/time/h6")).Text;
+                        var newsTopic = driver.FindElement(By.XPath("/html/body/main/div[1]/section/main/article[1]/div[2]/h3")).Text;
+                        var newsText = driver.FindElement(By.XPath("/html/body/main/div[1]/section/main/article[2]/div[2]")).Text;
+                        newsTime = new string(newsTime.Where(c => !char.IsPunctuation(c)).ToArray());
 
-                        string statementsDetails = $"{ti}\n\n + {no}";
+                        string statementsDetails = $"{newsTopic}\n\n + {newsText}";
                         Thread.Sleep(1000);
-                        File.WriteAllText($@"C:\Users\erfan\source\repos\LeaderProject\LeaderProject\{te}.txt", statementsDetails);
+                        File.WriteAllText($@"C:\Users\erfan\source\repos\LeaderProject\LeaderProject\{newsTime}.txt", statementsDetails);
                         Thread.Sleep(1000);
 
 
@@ -89,16 +89,16 @@ namespace Leader
                         }
 
 
-                        var t_Years = driver.FindElement(By.XPath($"/html/body/main/div[1]/section[1]/main/div[2]/ul/li[{i}]"));
+                        var selectedDefultYears = driver.FindElement(By.XPath($"/html/body/main/div[1]/section[1]/main/div[2]/ul/li[{i}]"));
                         Thread.Sleep(500);
 
-                        t_Years.Click();
+                        selectedDefultYears.Click();
 
 
                         Thread.Sleep(500);
-                        var sT_Month = driver.FindElement(By.XPath($"/html/body/main/div[1]/section[1]/main/div[3]/ul/li[{j}]"));
+                        var selectedDefultMonth = driver.FindElement(By.XPath($"/html/body/main/div[1]/section[1]/main/div[3]/ul/li[{j}]"));
 
-                        sT_Month.Click();
+                        selectedDefultMonth.Click();
                         Thread.Sleep(500);
                     }
                 }
